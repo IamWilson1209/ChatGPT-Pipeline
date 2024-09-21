183c765d8aeee11f86792ba0565bfe779fd079c1
IamWilson1209 <zenfonlee@gmail.com>
2024-09-21 08:25:41 -0400

    chatgpt

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    OpenAIAPIKey: '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'
+};
